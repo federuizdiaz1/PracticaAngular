ad2398fe511d8792f741b19175d68e664d6659d9
fecherico <federicoruizdiaz91@gmail.com>
2021-05-15 20:27:47 -0300

    creacion de nuevos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title:string = 'Feche <3';
+  title: string = 'Feche <3';
+  show: boolean = true;
+  sueldos = [1700, 1600, 1900];
+  fondo:any
+  activo:any
+  // showDos: boolean = true;
 }
